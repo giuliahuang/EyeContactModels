4af93ce1acb74a5d0a5119c971db83378b7ff238
Giulia Huang <huang@spitale-vm-1.inside.dei.polimi.it>
2024-12-28 23:53:24 +0000

    models for next speaker and eye contact

---
+++ FSFNetNP/README.md
@@ -0,0 +1,48 @@
+# FSFNet
+
+This work presents **FSFNet**, our solution for the eye contact detection task in MultiMediate2024.
+
+Fuyan Ma, Yiran He, Bin Sun, Shutao Li. [**"Less is More: Adaptive Feature Selection and Fusion for Eye Contact Detection"**](./assets/paper.pdf). ACM Multimedia 2024.
+
+![teaser](assets/method.png)
+
+## Dataset
+Please follow the instructions and download the MPIIGroupInteraction dataset in [the MultiMediate Challenge](https://multimediate-challenge.org/datasets/Dataset_MPII/).
+
+
+
+## Citation
+
+If you find this repo useful for your research, please consider citing the paper as follows:
+
+```bibtex
+@inproceedings{ma2024less,
+  title={Less is More: Adaptive Feature Selection and Fusion for Eye Contact Detection},
+  author={Ma, Fuyan and He, Yiran and Sun, Bin and Li, Shutao},
+  booktitle={Proceedings of the 32nd ACM International Conference on Multimedia},
+  note = {doi: {https://doi.org/10.1145/3664647.3688987.}}
+  year={2024}
+}
+
+@article{ma2021facial,
+  title={Facial expression recognition with visual transformers and attentional selective fusion},
+  author={Ma, Fuyan and Sun, Bin and Li, Shutao},
+  journal={IEEE Transactions on Affective Computing},
+  volume={14},
+  number={2},
+  pages={1236--1248},
+  year={2021},
+  publisher={IEEE}
+}
+
+@article{ma2023transformer,
+  title={Transformer-augmented network with online label correction for facial expression recognition},
+  author={Ma, Fuyan and Sun, Bin and Li, Shutao},
+  journal={IEEE Transactions on Affective Computing},
+  volume={15},
+  number={2},
+  pages={593--605},
+  year={2023},
+  publisher={IEEE}
+}
+```
---
+++ FSFNetNP/models/FSFNet.py
@@ -0,0 +1,87 @@
+import torch
+import numpy as np
+import torchvision
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torch.nn import functional as F
+
+from models.FSFEncoder import *
+from models.resnet50 import Backbone
+from utils.load_weights import load_pretrained_weights
+
+
+class CA_Module(nn.Module):
+    def __init__(self, input_dim: int):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(input_dim, input_dim)
+        self.relu = nn.ReLU()
+        self.linear2 = torch.nn.Linear(input_dim, input_dim)
+        self.sigmod = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.linear1(x)
+        x1 = self.relu(x1)
+        x1 = self.linear2(x1)
+        x1 = self.sigmod(x1)
+        x = x * x1
+        return x
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, input_dim: int, target_dim: int):
+        super().__init__()
+        self.linear = torch.nn.Linear(input_dim, target_dim)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        y_hat = self.linear(x)
+        return y_hat
+
+
+class FSFNet(nn.Module):
+    def __init__(self, img_size=224, num_classes=4, type="large"):
+        super().__init__()
+        if type == "small":
+            depth = 4
+        if type == "base":
+            depth = 6
+        if type == "large":
+            depth = 8
+
+        self.img_size = img_size
+        self.num_classes = num_classes
+
+
+
+        self.backbone = Backbone(50)
+        ir_checkpoint = torch.load('/workspace/home/huang/giulia/FSFNet/models/pretrain/ir50.pth', map_location=lambda storage, loc: storage)
+        self.backbone = load_pretrained_weights(self.backbone, ir_checkpoint)
+
+        self.fsf_encoder = FSFEncoder(in_chans=196, embed_dim=256,
+                                             depth=depth, num_heads=8, mlp_ratio=2.,
+                                             drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1, keep_rate=(0.6,))
+
+
+        self.ca = CA_Module(input_dim=256)
+        self.head = ClassificationHead(input_dim=256, target_dim=self.num_classes)
+
+    def forward(self, x):
+
+        # x: [batch_size, 4, C, H, W]
+        batch_size, num_imgs, C, H, W = x.size()
+        assert num_imgs == 4, "Number of images per sample must be 4."
+        # Reshape to process all images in the batch independently
+        x = x.view(batch_size * num_imgs, C, H, W)  # [B*4, C, H, W]
+
+        x_ir = self.backbone(x)
+        y_hat = self.fsf_encoder(x_ir)
+        y_hat = self.ca(y_hat)
+        y_feat = y_hat
+        out = self.head(y_hat)
+
+        out = out.view(batch_size, num_imgs)
+        y_feat = y_feat.view(batch_size, num_imgs, -1)
+        
+        return out, y_feat
+
+
